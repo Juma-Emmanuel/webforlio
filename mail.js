ef14c56ed8ab409bfa0d22e4f64c99539948a84d
Juma Emmanuel <jumaemmanuel462@yahoo.com>
2023-06-23 18:24:20 +0300

    added mail.js

---
+++ mail.js
@@ -0,0 +1,51 @@
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCeyATBXwuySMsCGxIk-7rUV-mS3p-qyE4",
+    authDomain: "contactform-fc473.firebaseapp.com",
+    databaseURL: "https://contactform-fc473-default-rtdb.firebaseio.com",
+    projectId: "contactform-fc473",
+    storageBucket: "contactform-fc473.appspot.com",
+    messagingSenderId: "1035070557982",
+    appId: "1:1035070557982:web:7094306e82429d3961430f"
+  };
+
+  // Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+var contactFormDB = firebase.database().ref("contactForm");
+document.getElementById("contactForm").addEventListener("submit", submitForm);
+
+function submitForm(e) {
+  e.preventDefault();
+
+  var name = getElementVal("name");
+  var email = getElementVal("email");
+  var message = getElementVal("message");
+
+  saveMessages(name, email, message);
+
+  //   enable alert
+  document.querySelector(".alert").style.display = "block";
+
+  //   remove the alert
+  setTimeout(() => {
+    document.querySelector(".alert").style.display = "none";
+  }, 3000);
+
+  //   reset the form
+  document.getElementById("contactForm").reset();
+}
+
+const saveMessages = (name, email, message) => {
+  var newContactForm = contactFormDB.push();
+
+  newContactForm.set({
+    name: name,
+    email: email,
+    message: message,
+  });
+};
+
+const getElementVal = (id) => {
+  return document.getElementById(id).value;
+};
